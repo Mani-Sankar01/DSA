a972d0e9d255edcab60c09208927bf4f4ba977a4
Mani-Sankar01 <sm7532567@gmail.com>
2024-07-25 11:20:42 +0530

    find reverse of a number

--- forloop.js
+++ forloop.js
@@ -1,4 +1,4 @@
-// find the sum of n
+// Q1. find the sum of n
 
 function findSum(n) {
   let sum = 0;
@@ -8,4 +8,16 @@ function findSum(n) {
   return sum;
 }
 
-console.log(findSum(10));
+// Q2. Find the reverse numver
+
+function findReverse(num) {
+  let newNum = 0;
+  while (num > 0) {
+    let last = num % 10;
+    newNum = newNum * 10 + last;
+    num = Math.floor(num / 10);
+  }
+  return newNum;
+}
+
+console.log(findReverse(123));
